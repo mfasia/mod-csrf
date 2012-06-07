ea1edd3eb080e8effd61ab059134167ac95e6fd4
pbuchbinder <pbuchbinder@73188386-4aad-4cb1-ada6-16f6dca2438c>
2012-06-07 18:27:24 +0000

    alpha release

---
+++ tags/0.0/test/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# -*-mode: ksh; ksh-indent: 2; -*-
+
+ROOT=`pwd`
+CUID=`id`
+CUID_STR=`expr "$CUID" : 'uid=[0-9]*.\([a-z,A-Z,0-9,_]*\)'`
+CUID=`expr "$CUID" : 'uid=\([0-9]*\)'`
+CUID=`expr $CUID '*' 10`
+CUID=`expr $CUID '%' 100`
+PORT_BASE=`expr ${CUID} + 5400`
+PORT_BASE1=`expr ${PORT_BASE} + 1`
+
+mkdir -p logs
+mkdir -p htdocs/doc
+rm htdocs/doc/*
+ln -s `pwd`/../doc/*.html htdocs/doc/
+ln -s `pwd`/../doc/*jpg htdocs/doc/
+
+FILES="conf/httpd.conf"
+for E in $FILES; do
+  sed <${E}.tmpl >${E} \
+    -e "s;##ROOT##;$ROOT;g" \
+    -e "s;##USR##;$CUID_STR;g" \
+    -e "s;##PORT_BASE##;$PORT_BASE;g" \
+    -e "s;##PORT_BASE1##;$PORT_BASE1;g"
+
+done
+
+echo "SET PORT_BASE=$PORT_BASE"    >  scripts/ports
+echo "SET PORT_BASE1=$PORT_BASE1" >>  scripts/ports
---
+++ tags/0.0/test/htdocs/scripts/ajaxsimple.js
@@ -0,0 +1,62 @@
+
+function autoStart() {
+  setInterval("autoUpdate()", 1000);
+  setInterval("autoSend()", 2000);
+}
+
+function autoSend() {
+  var client = new XMLHttpRequest();
+  client.onreadystatechange = sendHandler;
+  client.open("GET", "/update.xml?id=123456789");
+  client.send();
+}
+
+function sendHandler() {
+  if(this.readyState == this.DONE) {
+    if(this.status == 200 &&
+       this.responseXML != null) {
+      //var data = this.responseXML.getElementById("update");
+      var update = document.getElementById("autosend");
+      //update.innerHTML = data;
+      update.innerHTML = "== UPDATED from /update.xml == (" + this.responseText + ")";
+      return;
+    }
+  }
+}
+
+// automatically update the "autoupdate" div with the status fetched from the server
+function autoUpdate() {
+  var update = document.getElementById("autoupdate");
+  if(window.XMLHttpRequest) { // non-IE browsers
+    req = new XMLHttpRequest();
+    req.onreadystatechange = statusDiv;
+    try {
+      //req.open("GET", "/update.txt", true);
+      req.open("GET", "/update.txt?id=123", true);
+    } catch (e) {
+      // ignore
+      update.innerHTML = "FAILED!!!";
+    }
+    req.send(null);
+  } else if (window.ActiveXObject) { // IE
+    req = new ActiveXObject("Microsoft.XMLHTTP");
+    if (req) {
+      req.onreadystatechange = statusDiv;
+      req.open("GET", "/update.txt?id=123", true);
+      req.send();
+    }
+  }
+}
+
+// callback
+function statusDiv() {
+  if (req.readyState == 4) { // Complete
+    if (req.status == 200) { // OK response
+      var update = document.getElementById("autoupdate");
+      update.innerHTML = req.responseText;
+    } else{
+      var update = document.getElementById("autoupdate");
+      update.innerHTML = "FAILED!!!";
+    }
+  }
+}
