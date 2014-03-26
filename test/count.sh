a94ea9010861e940700062cab65fdd2a25d86e94
pbuchbinder <pbuchbinder@73188386-4aad-4cb1-ada6-16f6dca2438c>
2014-03-26 19:29:53 +0000

    test exp

--- test/count.sh
+++ test/count.sh
@@ -5,5 +5,8 @@ lines=`wc -l logs/access_log | awk '{print $1}'`
 kbytes=`expr $bytes / 1024`
 mbytes=`expr $kbytes / 1024`
 
-echo "$mbytes mbytes $lines requests"
+start=`head -1 logs/access_log | awk '{print $4}' | awk -F'[' '{print $2}'`
+end=`tail -1 logs/access_log | awk '{print $4}' | awk -F'[' '{print $2}'`
 
+echo "$start to $end"
+echo "$mbytes mbytes $lines requests"
